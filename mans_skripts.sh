ad249093a8907201d5cf5ca8ca04761bfc010112
user <paul.spakovskis@inbox.lv>
2018-10-01 11:49:29 +0300

    20181001_11_46

---
+++ mans_skripts.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+mkdir Mape
+cd Mape
+mkdir MapeMape
+# Skripts tiek izpildiīts ja tas apraksts ir atrodams caur  ./; ~/; /home/user/ vai caur $PATH - respektivi standarta ceļš
+# Otrkārt failam ir jābūt ar izpildišanas tiesībām
