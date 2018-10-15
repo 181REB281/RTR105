f81b5eda6f1be4bc2606e8ac9ee8afa7e99976a1
user <paul.spakovskis@inbox.lv>
2018-10-15 11:48:36 +0300

    20181015_11_48

---
+++ dgr_20181015.py
@@ -0,0 +1,246 @@
+Python 2.7.12 (default, Dec  4 2017, 14:50:18) 
+[GCC 5.4.0 20160609] on linux2
+Type "copyright", "credits" or "license()" for more information.
+>>> x = 12.2
+>>> y = 14
+>>> x = 100
+>>> vars()
+{'__builtins__': <module '__builtin__' (built-in)>, '__package__': None, 'x': 100, 'y': 14, '__name__': '__main__', '__doc__': None}
+>>> x1q3z9ocd = 35.0
+>>> x1q3z9afd = 12.50
+>>> x1q3z9afd
+12.5
+>>> x1q3z9afd = x1q3z9odc * x1q3z9afd
+
+Traceback (most recent call last):
+  File "<pyshell#7>", line 1, in <module>
+    x1q3z9afd = x1q3z9odc * x1q3z9afd
+NameError: name 'x1q3z9odc' is not defined
+>>> print(x1q3p9ad)x1q3z9afd = x1q3z9odc * x1q3z9afd
+SyntaxError: invalid syntax
+>>> x1q3z9afd = x1q3z9odc * x1q3z9afd
+
+Traceback (most recent call last):
+  File "<pyshell#9>", line 1, in <module>
+    x1q3z9afd = x1q3z9odc * x1q3z9afd
+NameError: name 'x1q3z9odc' is not defined
+\
+>>> x1q3z9afd = x1q3z9odc * x1q3z9afd
+
+Traceback (most recent call last):
+  File "<pyshell#10>", line 1, in <module>
+    x1q3z9afd = x1q3z9odc * x1q3z9afd
+NameError: name 'x1q3z9odc' is not defined
+>>> print(x1q3p9afd)
+
+Traceback (most recent call last):
+  File "<pyshell#11>", line 1, in <module>
+    print(x1q3p9afd)
+NameError: name 'x1q3p9afd' is not defined
+>>> x1q3p9afd = x1q3z9ocd * x1q3z9afd
+>>> print(x1q3p9afd)
+437.5
+>>> x = 2
+>>> x = x + 2
+>>> print(x)
+4
+>>> x = 5.3 * x * ( 1 - x )
+>>> x = 06
+>>> x = 0.6
+>>> print(x)
+0.6
+>>> x = 3.9 * x * ( 1 - x )
+>>> x = 3.9 * x * (1-x)
+>>> print(x)
+0.2336256
+>>> x
+0.2336256000000002
+>>> x
+0.2336256000000002
+>>> x = 0.6
+>>> x = 3.9 * x * (1-x)
+>>> print(x)
+0.936
+>>> xx = 2
+>>> xx = xx + 2
+>>> print(xx)
+4
+>>> yy = 440 * 12
+>>> print(yy)
+5280
+>>> zz = yy / 1000
+>>> print(zz)
+5
+>>> jj = 23
+>>> kk = jj % 5
+>>> print(kk)
+3
+>>> print(4 ** 3)
+64
+>>> eee = 'nu' + 'čau'
+>>> print(eee)
+nučau
+>>> eee = 'nu ' + 'čau'
+>>> print(eee)
+nu čau
+>>> eee = eee + 1
+
+Traceback (most recent call last):
+  File "<pyshell#44>", line 1, in <module>
+    eee = eee + 1
+TypeError: cannot concatenate 'str' and 'int' objects
+>>> type(eee)
+<type 'str'>
+>>> type('hello')
+<type 'str'>
+>>> type(1)
+<type 'int'>
+>>> print(float(99) + 100)
+199.0
+>>> i = 42
+>>> type(i)
+<type 'int'>
+>>> f = float(i)
+>>> print(f)
+42.0
+>>> sval = '123'
+>>> type(sval)
+<type 'str'>
+>>> print(sval + 1)
+
+Traceback (most recent call last):
+  File "<pyshell#55>", line 1, in <module>
+    print(sval + 1)
+TypeError: cannot concatenate 'str' and 'int' objects
+\
+>>> ival = int(sval)
+>>> type(ival)
+<type 'int'>
+>>> print(ival + 1)
+124
+>>> nsv = "hello bOb"
+>>> nam = input("who are you?")
+who are you?print('welcome'nam)
+
+Traceback (most recent call last):
+  File "<pyshell#74>", line 1, in <module>
+    nam = input("who are you?")
+  File "<string>", line 1
+    print('welcome'nam)
+        ^
+SyntaxError: invalid syntax
+>>> nam = input('who are you?'°
+	    
+SyntaxError: invalid syntax
+>>> nam = input('who are you?')
+who are you?
+
+Traceback (most recent call last):
+  File "<pyshell#76>", line 1, in <module>
+    nam = input('who are you?')
+  File "<string>", line 0
+    
+    ^
+SyntaxError: unexpected EOF while parsing
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> clear
+
+Traceback (most recent call last):
+  File "<pyshell#83>", line 1, in <module>
+    clear
+NameError: name 'clear' is not defined
+>>> inp = input('Europe floor?)
+	    
+SyntaxError: EOL while scanning string literal
+>>> inp = input('Europe floor?')
+Europe floor?
+
+Traceback (most recent call last):
+  File "<pyshell#85>", line 1, in <module>
+    inp = input('Europe floor?')
+  File "<string>", line 0
+    
+    ^
+SyntaxError: unexpected EOF while parsing
+>>> input("vadi skaitli")
+vadi skaitli 3
+3
+>>> o = input("vadi skaitli")
+vadi skaitli5
+>>> o
+5
+>>> usf = int(inp) + 1
+
+Traceback (most recent call last):
+  File "<pyshell#89>", line 1, in <module>
+    usf = int(inp) + 1
+NameError: name 'inp' is not defined
+>>> usf = int ( inp ) + 1
+
+Traceback (most recent call last):
+  File "<pyshell#90>", line 1, in <module>
+    usf = int ( inp ) + 1
+NameError: name 'inp' is not defined
+>>> usf = int (inp) + 1
+
+Traceback (most recent call last):
+  File "<pyshell#91>", line 1, in <module>
+    usf = int (inp) + 1
+NameError: name 'inp' is not defined
+>>> usf = int(inp) + 1
+
+Traceback (most recent call last):
+  File "<pyshell#92>", line 1, in <module>
+    usf = int(inp) + 1
+NameError: name 'inp' is not defined
+>>> inp = input("Europe floor?")
+Europe floor? 0
+>>> usf = int(inp) + 1
+>>> print("US floor", usf)
+('US floor', 1)
+>>> print('US floor', usf)
+('US floor', 1)
+>>> print('US floor', usf)
+('US floor', 1)
+>>> name = input('Enter file:')
+Enter file:jekaterinburga
+
+Traceback (most recent call last):
+  File "<pyshell#98>", line 1, in <module>
+    name = input('Enter file:')
+  File "<string>", line 1, in <module>
+NameError: name 'jekaterinburga' is not defined
+>>> handle = open(name, 'r')
+
+Traceback (most recent call last):
+  File "<pyshell#99>", line 1, in <module>
+    handle = open(name, 'r')
+NameError: name 'name' is not defined
+>>> name = input('Enter file:')
+Enter file: 44
+>>> handle = open(name, 'r')
+
+Traceback (most recent call last):
+  File "<pyshell#101>", line 1, in <module>
+    handle = open(name, 'r')
+TypeError: coercing to Unicode: need string or buffer, int found
+>>> handle = open(name,'r')
+
+Traceback (most recent call last):
+  File "<pyshell#102>", line 1, in <module>
+    handle = open(name,'r')
+TypeError: coercing to Unicode: need string or buffer, int found
+>>> handle = open(name, 'r')
+
+Traceback (most recent call last):
+  File "<pyshell#103>", line 1, in <module>
+    handle = open(name, 'r')
+TypeError: coercing to Unicode: need string or buffer, int found
+>>> 
+
+
